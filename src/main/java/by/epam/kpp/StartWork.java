8b5c80756e8c375256a241572e3732393bfd99ef
Yan <45950020+sidorovichyan@users.noreply.github.com>
2022-04-15 20:05:01 +0300

    Update

--- src/main/java/by/epam/kpp/StartWork.java
+++ src/main/java/by/epam/kpp/StartWork.java
@@ -13,7 +13,6 @@ public class StartWork {
 
     public static void main(String[] args) {
         logger.info("Start application");
-        System.out.println("hello");
         SpringApplication.run(StartWork.class, args);
     }
 }
